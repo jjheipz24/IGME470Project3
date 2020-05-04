fbf16fc1cd3dbb993aa6d87d8574fad1c8f5a1e8
Jin Jin Heipler <heipler.jj@gmail.com>
2020-05-04 19:04:07 -0400

    app.js sends message to arduino

--- server/app.js
+++ server/app.js
@@ -1,29 +1,47 @@
 const SerialPort = require('serialport');
 const config = require('../config.js');
 
-const sp = new SerialPort("COM3", {
+const sp = new SerialPort('COM3', {
     baudrate: config.baudRate,
 }, false);
 
 console.log('Starting up serial host...');
 
-const message = 'DATA GOES HERE';
+const message = "1\n";
 
-function write() {
-    sp.open((err) => {
-        console.log(`Writing serial data: ${message}`);
+// function write() {
+//     sp.open((err) => {
+//         console.log(`Writing serial data: ${message}`);
+//         sp.write(message, (err, res) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             //sp.close();
+//         });
+//     });
+// }
+
+// setTimeout(write, 1000); // wait 1s for everything to initialize correctly
+// setInterval(write, 2000); // write data every 5s
+
+// module.exports = {
+//     write: write
+// };
+
+sp.on("open", () => {
+    console.log("port opened");
+    setTimeout(() => {
         sp.write(message, (err, res) => {
-            if (err) {
+            if(err){
                 console.log(err);
             }
-            sp.close();
-        });
-    });
-}
-
-setTimeout(write, 1000); // wait 1s for everything to initialize correctly
-setInterval(write, 5000); // write data every 5s
-
-module.exports = {
-    write: write
-};
+        })
+    }, 2000);
+    setInterval(() => {
+        sp.write(message, (err, res) => {
+            if(err){
+                console.log(err);
+            }
+        })
+    }, 3000);
+});
