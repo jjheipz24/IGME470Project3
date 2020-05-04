99e2435f3036dbca05d5242af108654c8838c16b
Jin Jin Heipler <heipler.jj@gmail.com>
2020-05-04 19:39:32 -0400

    light blinks on and off

--- server/app.js
+++ server/app.js
@@ -9,39 +9,54 @@ console.log('Starting up serial host...');
 
 const message = "1\n";
 
-// function write() {
-//     sp.open((err) => {
-//         console.log(`Writing serial data: ${message}`);
+function on() {
+    sp.open((err) => {
+        //console.log(`Writing serial data: ${message}`);
+        sp.write(message, (err, res) => {
+            if (err) {
+                console.log(err);
+            }
+            //sp.close();
+        });
+    });
+}
+
+function off(){
+    sp.open((err) => {
+        //console.log(`Writing serial data: ${message}`);
+        sp.write("0\n", (err, res) => {
+            if (err) {
+                console.log(err);
+            }
+            //sp.close();
+        });
+    });
+}
+
+setTimeout(off, 1000); // wait 1s for everything to initialize correctly
+setInterval(on, 2000); // write data every X seconds
+setInterval(off, 2000); // write data every X seconds
+
+module.exports = {
+    on: on,
+    off: off
+};
+
+// sp.on("open", () => {
+//     console.log("port opened");
+//     setTimeout(() => {
 //         sp.write(message, (err, res) => {
-//             if (err) {
+//             if(err){
 //                 console.log(err);
 //             }
-//             //sp.close();
-//         });
-//     });
-// }
+//         })
+//     }, 1000);
+//     setInterval(() => {
+//         sp.write(message, (err, res) => {
+//             if(err){
+//                 console.log(err);
+//             }
+//         })
+//     }, 2000);
 
-// setTimeout(write, 1000); // wait 1s for everything to initialize correctly
-// setInterval(write, 2000); // write data every 5s
-
-// module.exports = {
-//     write: write
-// };
-
-sp.on("open", () => {
-    console.log("port opened");
-    setTimeout(() => {
-        sp.write(message, (err, res) => {
-            if(err){
-                console.log(err);
-            }
-        })
-    }, 2000);
-    setInterval(() => {
-        sp.write(message, (err, res) => {
-            if(err){
-                console.log(err);
-            }
-        })
-    }, 3000);
-});
+// });
