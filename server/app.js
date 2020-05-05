593b374481148cce9b6c7c1b831f70ecb0529060
Jin Jin Heipler <heipler.jj@gmail.com>
2020-05-04 21:46:44 -0400

    Message changes the animation of neopixel

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const sp = new SerialPort('COM3', {
 
 console.log('Starting up serial host...');
 
-const message = "1\n";
+const message = "A=304\n";
 
 function on() {
     sp.open((err) => {
@@ -33,30 +33,11 @@ function off(){
     });
 }
 
-setTimeout(off, 1000); // wait 1s for everything to initialize correctly
+//setTimeout(on, 1000); // wait 1s for everything to initialize correctly
 setInterval(on, 2000); // write data every X seconds
-setInterval(off, 2000); // write data every X seconds
+//setInterval(off, 2000); // write data every X seconds
 
 module.exports = {
     on: on,
     off: off
 };
-
-// sp.on("open", () => {
-//     console.log("port opened");
-//     setTimeout(() => {
-//         sp.write(message, (err, res) => {
-//             if(err){
-//                 console.log(err);
-//             }
-//         })
-//     }, 1000);
-//     setInterval(() => {
-//         sp.write(message, (err, res) => {
-//             if(err){
-//                 console.log(err);
-//             }
-//         })
-//     }, 2000);
-
-// });
